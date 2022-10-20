e6df64de2606d2c6ef4eaeb5cc1329fb9cc34d89
Steven Vu <sssstevenvu@gmail.com>
2022-10-19 19:02:38 -0700

    Initialized all page files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,5 @@
 export { default as Home } from './home/Home';
+export { default as Alphabet } from './alphabet/Alphabet';
+export { default as Tones } from './tones/Tones';
+export { default as Clusters } from './clusters/Clusters';
+export { default as Vocab } from './vocab/Vocab';

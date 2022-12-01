15dc8567efaa67842c743af6fc395f04830e9b5f
Steven Vu <sssstevenvu@gmail.com>
2022-11-30 18:41:24 -0800

    Updated Vocab Page
    Only thing missing is audio

--- src/pages/vocab/Vocab.js
+++ src/pages/vocab/Vocab.js
@@ -9,6 +9,11 @@ import { bgm } from './vocabAudio';
 import { vocabBg } from '../../assets/images';
 import { motion } from 'framer-motion';
 
+// Import json file
+import alphabetData from '../../data/alphabet.json';
+import clusterData from '../../data/clusters.json';
+import toneData from '../../data/tones.json';
+
 const Vocab = () => {
     return(
         <motion.div className='vocab-container'
@@ -22,10 +27,65 @@ const Vocab = () => {
                 playing={true}
                 loop={true}
             />
-
-            <h1>Alphabet Page</h1>
-            <h1>Clusters Page</h1>
-            <h1>Tones Page</h1>
+            <div className='vocab-lists'>
+                <h1>Alphabet Page</h1>
+                <table>
+                    <tr>
+                        <th>Letter</th>
+                        <th>Viet Example</th>
+                        <th>English Example</th>
+                    </tr>
+                    {alphabetData.map((alphabet, index) => {
+                        return(
+                            <tr key={index}>
+                                <th>{alphabet.letter}</th>
+                                <th>{alphabet.vietEx}</th>
+                                <th>{alphabet.engEx}</th>
+                            </tr>
+                        )
+                    })}
+                </table>
+                <h1>Clusters Page</h1>
+                <table>
+                    <tr>
+                        <th>Combo</th>
+                        <th>Viet Example</th>
+                        <th>English Example</th>
+                        <th>Type</th>
+                    </tr>
+                    {clusterData.map((cluster, index) => {
+                        return(
+                            <tr key={index}>
+                                <th>{cluster.combo}</th>
+                                <th>{cluster.vietEx}</th>
+                                <th>{cluster.engEx}</th>
+                                <th>{cluster.type}</th>
+                            </tr>
+                        )
+                    })}
+                </table>
+                <h1>Tones Page</h1>
+                <table>
+                    <tr>
+                        <th>Symbol Name</th>
+                        <th>Symbol</th>
+                        <th>Explanation</th>
+                        <th>Viet Example</th>
+                        <th>English Translation</th>
+                    </tr>
+                    {toneData.map((tone, index) => {
+                        return(
+                            <tr key={index}>
+                                <th>{tone.tonesymbolname}</th>
+                                <th>{tone.symbol}</th>
+                                <th>{tone.explanation}</th>
+                                <th>{tone.vietEx}</th>
+                                <th>{tone.engEx}</th>
+                            </tr>
+                        )
+                    })}
+                </table>
+            </div>
         </motion.div>
     );
 };
